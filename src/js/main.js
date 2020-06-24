c8a0dce8a092004d49eb73fcc8e803145c9d50c0
Yevhen <webstyle.ugn@gmail.com>
2020-06-24 19:15:41 +0300

    final project ready

--- src/js/main.js
+++ src/js/main.js
@@ -52,7 +52,56 @@ window.addEventListener('DOMContentLoaded', () => {
         setClock(id, endtime);
     }
 
-    timer('.trigger__timer', '2020-06-15');
-    timer('.timer-bottom', '2020-06-15');
+    const mask = (selector) => {
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+    
+            if (elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+    
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+    
+        function createMask(event) {
+            let matrix = '+38 (___) ___ __ __',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, '');
+    
+            if (def.length >= val.length) {
+                val = def;
+            }
+    
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+    
+            if (event.type === 'blur') {
+                if (this.value.length == 3) {
+                    this.value = '';
+                }
+            } else {
+                setCursorPosition(this.value.length, this);
+            }
+        }
+    
+        let inputs = document.querySelectorAll(selector);
+    
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
+    };
+
+    timer('.trigger__timer', '2020-06-30');
+    timer('.timer-bottom', '2020-06-30');
+    mask('input[name="phone"]');
 
 });
